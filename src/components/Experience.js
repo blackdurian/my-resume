ef56574d1a2b2557ee8423b66c12931781fae4a9
lwqec <33524888+blackdurian@users.noreply.github.com>
2022-03-01 03:29:13 +0800

    deleted image form public

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,7 +8,7 @@ return(
     <h4>Experience</h4>
      <Company
          website="https://www.crimsonlogic.com/"
-         icon="https://media-exp1.licdn.com/dms/image/C560BAQHi8-y-aczHwg/company-logo_100_100/0/1640076331233?e=1654128000&v=beta&t=k1bs64WQzR6OX0uPQ3UJ2SnpvIx_O5nSZJDbJoJ1gNU"
+         icon="https://raw.githubusercontent.com/blackdurian/my-resume/gh-pages/company-icon/crimsomlogic.jpg"
          jobTitle='Software Engineer'
          name='CrimsonLogic Pte Ltd'
          jobLevel='Internship'
@@ -16,8 +16,8 @@ return(
          description= {`- Be part of Coherence Data Exchange Platform (DXP) product team to build a product that publisher can publish Datasets and made available for use to subscribers. \n- Contributed on bug fixes, features development, automation test, and function research.`}
      />
           <Company
-         website="https://www.crimsonlogic.com/"
-         icon='/yonyou.jpg'
+         website="https://yonyou.com.my/"
+         icon='https://raw.githubusercontent.com/blackdurian/my-resume/gh-pages/company-icon/yonyou.jpg'
          jobTitle='Assistant Developer'
          name='Yonyou (Malaysia) Sdn Bhd'
          jobLevel='Internship'
@@ -26,12 +26,12 @@ return(
      />
           <Company
          website="https://www.crimsonlogic.com/"
-         icon="https://media-exp1.licdn.com/dms/image/C560BAQHi8-y-aczHwg/company-logo_100_100/0/1640076331233?e=1654128000&v=beta&t=k1bs64WQzR6OX0uPQ3UJ2SnpvIx_O5nSZJDbJoJ1gNU"
-         jobTitle='Software Engineer'
-         name='CrimsonLogic Pte Ltd'
+         icon="https://raw.githubusercontent.com/blackdurian/my-resume/gh-pages/company-icon/dieboldnixdorf.jpg"
+         jobTitle='Quality Assurance Tester'
+         name='Diebold Nixdorf'
          jobLevel='Internship'
-         periodStr='Dec 2020 - Apr 2021 · 4 mos'
-         description= {`- Be part of Coherence Data Exchange Platform (DXP) product team to build a product that publisher can publish Datasets and made available for use to subscribers. \n- Contributed on bug fixes, features development, automation test, and function research. `}
+         periodStr='Sep 2019 - Dec 2019 · 3 mos'
+         description= {`Developing test cases and verification of the test environment based on project's requirements was done on daily basis. It included designing and executing test scripts.`}
      />
     </div>);
 };
